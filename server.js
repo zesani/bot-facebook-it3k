f3a787e0409361afbdaf1e1083340467b5918e29
zesani <7sin@outlook.co.th>
2016-11-21 21:21:12 +0700

    react

--- server.js
+++ server.js
@@ -1,44 +1,41 @@
 var express = require('express')
 var bodyParser = require('body-parser')
+var request = require('request')
 var app = express()
-var cors = require('cors')
 
-app.use(cors())
 app.use(bodyParser.json())
-
 app.set('port', (process.env.PORT || 4000))
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
-app.get('/webhook', function (req, res) {
-  var key = 'EAAasXgVHJYYBAM23KldGjZBmdMDtfN8g7CdL6k4ONqILUDlE246o8rohoqXscqzxnuAamChP6CFoOxU2AncMeiqg8YctT2rE3kGZANP7OEgakz2OKzutP5ZCaJZBOCeuDi1s7nZAN24eWDzRtS3pEsSDZCToCvFRkXJzsjFMZCbrwZDZD'
-  if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === key) {
-    console.log('Validating webhook')
-    res.status(200).send(req.query['hub.challenge'])
-  } else {
-    console.error('Failed validation. Make sure the validation tokens match.')
-    res.sendStatus(403)
+app.get('/webhook', function(req, res) {
+  var key = 'EAAD98d4jaMABALd9uZAFMbllQWZCOwZBtjPEkHcZCMrk050ZAjgsTZBIgsprI41nXR8XomBMCUPnhcDQZCMz1Rlyhaz0Vjq1JxlGuV4qcbPu38wpIZCDErky0PupzFJkpk7oqR7uoJaNivR4llCJ8MNLnZA4unhWnZBWkLMabNfxuKOQZDZD'
+  if (req.query['hub.verify_token'] === key) {
+    res.send(req.query['hub.challenge'])
   }
+  res.send('Error, wrong token')
 })
 
 app.post('/webhook', function (req, res) {
   var data = req.body
+
   // Make sure this is a page subscription
   if (data.object === 'page') {
+
     // Iterate over each entry - there may be multiple if batched
-    data.entry.forEach(function (entry) {
+    data.entry.forEach(function(entry) {
       var pageID = entry.id
       var timeOfEvent = entry.time
+
       // Iterate over each messaging event
-      entry.messaging.forEach(function (event) {
+      entry.messaging.forEach(function(event) {
         if (event.message) {
           receivedMessage(event)
         } else {
-          console.log('Webhook received unknown event: ', event)
+          console.log("Webhook received unknown event: ", event)
         }
-      })
-    })
+      });
+    });
 
     // Assume all went well.
     //
@@ -47,9 +44,9 @@ app.post('/webhook', function (req, res) {
     // will time out and we will keep trying to resend.
     res.sendStatus(200)
   }
-})
+});
 
-function receivedMessage (event) {
+function receivedMessage(event) {
   var senderID = event.sender.id
   var recipientID = event.recipient.id
   var timeOfMessage = event.timestamp
@@ -83,8 +80,7 @@ function receivedMessage (event) {
     sendTextMessage(senderID, "Message with attachment received");
   }
 }
-
-function sendGenericMessage (recipientId, messageText) {
+function sendGenericMessage(recipientId, messageText) {
   // To be expanded in later sections
 }
 
@@ -96,32 +92,33 @@ function sendTextMessage(recipientId, messageText) {
     message: {
       text: messageText
     }
-  }
+  };
 
-  callSendAPI(messageData)
+  callSendAPI(messageData);
 }
 
-function callSendAPI (messageData) {
+function callSendAPI(messageData) {
   request({
     uri: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: { access_token: 'EAAasXgVHJYYBAM23KldGjZBmdMDtfN8g7CdL6k4ONqILUDlE246o8rohoqXscqzxnuAamChP6CFoOxU2AncMeiqg8YctT2rE3kGZANP7OEgakz2OKzutP5ZCaJZBOCeuDi1s7nZAN24eWDzRtS3pEsSDZCToCvFRkXJzsjFMZCbrwZDZD' },
+    qs: { access_token: 'EAAD98d4jaMABALd9uZAFMbllQWZCOwZBtjPEkHcZCMrk050ZAjgsTZBIgsprI41nXR8XomBMCUPnhcDQZCMz1Rlyhaz0Vjq1JxlGuV4qcbPu38wpIZCDErky0PupzFJkpk7oqR7uoJaNivR4llCJ8MNLnZA4unhWnZBWkLMabNfxuKOQZDZD' },
     method: 'POST',
     json: messageData
 
   }, function (error, response, body) {
-    if (!error && response.statusCode === 200) {
-      var recipientId = body.recipient_id
-      var messageId = body.message_id
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
 
       console.log("Successfully sent generic message with id %s to recipient %s",
         messageId, recipientId);
     } else {
-      console.error("Unable to send message.")
-      console.error(response)
-      console.error(error)
+      console.error("Unable to send message.");
+      console.error(response);
+      console.error(error);
     }
-  })
+  });
 }
+
 app.listen(app.get('port'), function () {
   console.log('run at port', app.get('port'))
 })
